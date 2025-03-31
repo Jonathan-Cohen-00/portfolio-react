772151c5c526201c7cffe71425d3734fed3a3d7a
Jonathan <jonathancohen118@gmail.com>
2025-03-31 05:00:53 +0200

    Webp bg

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
-import homeBg from "../../Assets/home-bg.jpg"; 
+import homeBg from "../../Assets/home-bg.webp"; 
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
