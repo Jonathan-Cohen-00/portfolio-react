5302d8341fa3378e155e7f9c46a104b5d615ca68
Jonathan <jonathancohen118@gmail.com>
2025-03-31 02:55:38 +0200

    Clean

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/jonathan_cohen/portfolio/',
-  assetsInclude: ['**/*.worker.js'],
-
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Portfolio Jonathan Cohen',
+        short_name: 'Portfolio',
+        start_url: '/jonathan_cohen/portfolio/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon'
+          }
+        ]
+      }
+    })
+  ],
+  assetsInclude: ['**/*.worker.js']
 })
